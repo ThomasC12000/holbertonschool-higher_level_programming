771a854c8e3c5bf31509aab891b996b01bd9c044
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-05-17 15:32:51 +0200

    make task 5 and 6

---
+++ python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""this script write a class Square that defines a square """
+
+
+class Square:
+    """this class defines a square"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if isinstance(value, tuple) and len(value) == 2 \
+                and all(isinstance(elem, int) and elem >= 0 for elem in value):
+            self.__position = value
+        else:
+            raise TypeError("position must be a tuple of 2 positive integers")
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.position[1]):
+                print()
+            for _ in range(0, self.size):
+                for _ in range(self.position[0]):
+                    print(" ", end='')
+                for _ in range(self.size):
+                    print("#", end='')
+                print()
