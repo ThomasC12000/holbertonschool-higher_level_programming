a7ba13ecbf7a66bc4cfcdb165fb81cbbc8be0431
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-05-17 09:48:34 +0200

    make task 2

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""building the class square"""
+
+
+class Square:
+    """empty classe"""
+    def __init__(self, size=0):
+        """this is the constructor"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
