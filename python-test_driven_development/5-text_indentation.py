476f9f62799352c56a210b0512cdc832e65e5df7
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-05-21 14:14:40 +0200

    make task 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''Function that indent a text after each . : or ? character'''
+
+
+def text_indentation(text):
+    '''Indent the input string by adding new lines and removing spaces'''
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    prev = False
+    for char in text:
+        if char == ' ' and prev:
+            prev = False
+            continue
+        elif char in (".", "?", ":"):
+            print(char)
+            print()
+            prev = True
+        else:
+            print(char, end="")
