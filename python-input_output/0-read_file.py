50559de87f6924eef66cb4237e118efe44b6487a
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-05-27 09:57:08 +0200

    update file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""module for read text file"""
+'''module for read text file'''
 
 
 def read_file(filename=""):
-    """Reads a UTF-8 text file and displays it on the screen"""
-
+    '''Reads a UTF-8 text file and displays it on the screen'''
 
     with open(filename, "r", encoding="utf-8") as file:
         print(file.read())
