53f0da72542a316cdb26f9e50213fe70bd5840eb
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-05-27 11:10:21 +0200

    update task 6 and make task 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+'''MODULE'''
+import json
+
+
+def class_to_json(obj):
+    '''
+    Write a function that returns the dictionary description with simple data
+    structure (list, dictionary, string, integer and boolean)
+    for JSON serialization of an object
+    '''
+
+    json_dict = {}
+    for attr in dir(obj):
+        if not attr.startswith("__") and not callable(getattr(obj, attr)):
+            value = getattr(obj, attr)
+            if isinstance(value, (list, dict, str, int, bool)):
+                json_dict[attr] = value
+    return json_dict
