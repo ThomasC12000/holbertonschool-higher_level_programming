193ffbc222d8c610359baeb5352b62f749b3473c
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-05-27 14:32:38 +0200

    make all files and README.md

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''MODULE'''
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """Serialize and save data to the specified file."""
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+def load_and_deserialize(filename):
+    """Load and deserialize data from the specified file."""
+    with open(filename, 'r') as file:
+        return json.load(file)
