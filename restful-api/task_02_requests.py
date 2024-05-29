9cecf51b76c5426f56b2ca1a301735f999ab03de
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-05-29 10:05:14 +0200

    create directory, README.md and make task 2

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,28 @@
+import requests
+import csv
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    print(f"Status Code: {response.status_code}")
+    
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        posts = response.json()
+        
+        posts_list = [{'id': post['id'], 'title': post['title'], 'body': post['body']} for post in posts]
+        
+        with open('posts.csv', mode='w', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=['id', 'title', 'body'])
+            writer.writeheader()
+            for post in posts_list:
+                writer.writerow(post)
