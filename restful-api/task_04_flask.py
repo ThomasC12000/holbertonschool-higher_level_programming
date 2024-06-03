ff0926e9aa6764adf1d035eb432dbe07c3b472f0
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-06-03 15:34:03 +0200

    update task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -1,21 +1,13 @@
-from flask import Flask, jsonify, request
+#!/usr/bin/python3
+'''4. Develop a Simple API using Python with Flask'''
+
+
+from flask import Flask
+from flask import jsonify
+from flask import request
 
 app = Flask(__name__)
-
-users = {
-    "jane": {
-        "username": "jane",
-        "name": "Jane",
-        "age": 28,
-        "city": "Los Angeles"
-    },
-    "john": {
-        "username": "john",
-        "name": "John",
-        "age": 30,
-        "city": "New York"
-    }
-}
+users = {}
 
 
 @app.route('/')
@@ -24,7 +16,7 @@ def home():
 
 
 @app.route('/data')
-def data():
+def get_data():
     return jsonify(list(users.keys()))
 
 
@@ -32,24 +24,28 @@ def data():
 def status():
     return "OK"
 
-
 @app.route('/users/<username>')
-def get_user(username):
-    user = users.get(username)
-    if user:
-        return jsonify(user)
-    else:
-        return jsonify({"error": "User not found"}), 404
+def get_users(username):
+    if username in users:
+        return jsonify(users[username])
+    return jsonify({"error": "User not found"}), 404
 
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    user_data = request.get_json()
-    username = user_data.get('username')
-    if username in users:
-        return jsonify({"error": "User already exists"}), 400
-    users[username] = user_data
-    return jsonify({"message": "User added", "user": user_data}), 201
+    if not request.json or "username" not in request.json:
+        return jsonify({"error": "Username is required"}), 400
+    user_data = request.json
+    username = user_data["username"]
+    # if username in users:
+    #     return jsonify({"error": "User already exists"}), 400
+    users[username] = {
+        "username": user_data.get("username"),
+        "name": user_data.get("name"),
+        "age": user_data.get("age"),
+        "city": user_data.get("city")
+    }
+    return jsonify({"message": "User added", "user": users[username]}), 201
 
 
 if __name__ == "__main__":
