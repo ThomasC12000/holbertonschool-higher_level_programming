f7a5f927b7b31dd7830b42009ec8af13e11c2ced
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-06-18 14:26:08 +0200

    update privileges for user_0d_1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -2,7 +2,7 @@
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- give privileges to user user_0d_1
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 
 -- apply modification
 FLUSH PRIVILEGES;
