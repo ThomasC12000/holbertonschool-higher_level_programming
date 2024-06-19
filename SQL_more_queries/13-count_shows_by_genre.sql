8ee3acf8cdf900d10ac35fd8737937a891297810
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-06-19 15:27:58 +0200

    make task 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,7 @@
 -- lists all shows contained in hbtn_0d_tvshows
-SELECT tv_genres.name AS genre, tv_genres.id AS number_of_shows
-FROM tv_genres, tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_show_genres.genre_id DESC;
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
+FROM tv_genres
+JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
