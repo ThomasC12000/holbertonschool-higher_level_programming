bef37c3797ced0ea8e06ec196d41664b0583b233
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-06-19 09:35:47 +0200

    update task 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
--- use database hbtn_0d_usa
+-- use database hbtn_0d_usa 
 USE hbtn_0d_usa;
 
 -- lists all the cities of California
 SELECT id, name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
+ORDER BY state_id ASC;
