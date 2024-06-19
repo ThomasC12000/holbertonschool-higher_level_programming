8de080c784655f03d590f1758aaa9acb372f796e
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-06-19 10:32:56 +0200

    make task 10

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,6 @@
+USE hbtn_0d_tvshows;
+
+-- lists all shows contained in hbtn_0d_tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows, tv_show_genres
+ORDER BY tv_shows.title, tv_show_genres.genre_id
