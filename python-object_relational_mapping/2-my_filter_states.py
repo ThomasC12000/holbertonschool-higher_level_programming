be6e1ed7f2e34be420645748f3acb6e4b5ebfc00
ThomasC12000 <thomas.chlpac.pro@hotmail.com>
2024-06-21 09:04:50 +0200

    update task 2 and make 3

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     db_name = sys.argv[3]
-    state_name = sys.argv[4]
+    state_name_searched = sys.argv[4]
 
     db = MySQLdb.connect(
         host="localhost",
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     query = "SELECT * FROM states WHERE \
-        name = '{}' ORDER BY id ASC".format(state_name)
+        name = '{}' ORDER BY states.id ASC".format(state_name_searched)
     cursor.execute(query)
 
     states = cursor.fetchall()
